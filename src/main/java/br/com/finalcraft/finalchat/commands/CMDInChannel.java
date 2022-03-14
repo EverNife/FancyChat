1f865390816f1f87a81ce9a698206405fc8e9e7b
Petrus <petrus_ant@hotmail.com>
2022-03-14 06:43:15 -0300

    Make CMDInChannel use new FinalCMD System

--- src/main/java/br/com/finalcraft/finalchat/commands/CMDInChannel.java
+++ src/main/java/br/com/finalcraft/finalchat/commands/CMDInChannel.java
@@ -1,81 +1,54 @@
 package br.com.finalcraft.finalchat.commands;
 
 
-import br.com.finalcraft.evernifecore.util.FCBukkitUtil;
-import br.com.finalcraft.finalchat.FinalChat;
+import br.com.finalcraft.evernifecore.argumento.MultiArgumentos;
+import br.com.finalcraft.evernifecore.commands.finalcmd.ICustomFinalCMD;
+import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.FinalCMD;
+import br.com.finalcraft.evernifecore.commands.finalcmd.executor.CustomizeContext;
+import br.com.finalcraft.evernifecore.util.FCScheduller;
 import br.com.finalcraft.finalchat.config.fancychat.FancyChannel;
-import br.com.finalcraft.finalchat.config.fancychat.FancyChannelController;
 import br.com.finalcraft.finalchat.util.ChannelManager;
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-public class CMDInChannel implements CommandExecutor {
+public class CMDInChannel implements ICustomFinalCMD {
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        inChannel(label,sender,args);
-        return true;
+    private final FancyChannel fancyChannel;
+
+    public CMDInChannel(FancyChannel fancyChannel) {
+        this.fancyChannel = fancyChannel;
     }
 
-    // -----------------------------------------------------------------------------------------------------------------------------//
-    // Command InChannel
-    // -----------------------------------------------------------------------------------------------------------------------------//
-    public static boolean inChannel(String label, CommandSender sender, String[] args){
+    @Override
+    public void customize(@NotNull CustomizeContext context) {
+        context.getFinalCMDData()
+                .permission(fancyChannel.getPermission())
+                .labels(
+                        new String[]{fancyChannel.getName(), fancyChannel.getAlias()}
+                );
+    }
 
-        if (FCBukkitUtil.isNotPlayer(sender)){
-            return true;
-        }
+    @FinalCMD(
+            aliases = "",
+            usage = "[msg]"
+    )
+    public void inChannel(Player player, MultiArgumentos argumentos){
 
-        Player player = (Player) sender;
-
-        List<FancyChannel> allFancyChannels = new ArrayList<FancyChannel>(FancyChannelController.mapOfFancyChannels.values());
-        List<String> allAliases = new ArrayList<String>();
-        allFancyChannels.forEach(fancyChannel -> {
-            allAliases.add(fancyChannel.getAlias().toLowerCase());
-        });
-
-        FancyChannel fancyChannel = null;
-        int amoutOfChannels = allFancyChannels.size();
-        for (int i = 0; i < amoutOfChannels; i++){
-            if (allAliases.get(i).equals(label)){
-                fancyChannel = allFancyChannels.get(i);
-                break;
-            }
-        }
-
-        if (!fancyChannel.getPermission().isEmpty()){
-            if (!FCBukkitUtil.hasThePermission(player,fancyChannel.getPermission())){
-                return true;
-            }
-        }
-
-        String msg = String.join(" ", args).trim();
-
-        if (msg.isEmpty()){
-            ChannelManager.setPlayerLockChannel(player,fancyChannel);
-            sender.sendMessage("§6§l ▶ §aCanal §e[" + fancyChannel.getName() + "]§a definido como padrão!");
-            return true;
+        if (argumentos.get(0).isEmpty()){
+            ChannelManager.setPlayerLockChannel(player, fancyChannel);
+            player.sendMessage("§6§l ▶ §aCanal §e[" + fancyChannel.getName() + "]§a definido como padrão!");
+            return;
         }
 
         ChannelManager.setTempChannel(player,fancyChannel);
         Set<Player> onlinePlayer = new HashSet(Bukkit.getOnlinePlayers());
-        AsyncPlayerChatEvent event = new AsyncPlayerChatEvent(true, player, msg, onlinePlayer);
-        new BukkitRunnable(){
-            @Override
-            public void run() {
-                Bukkit.getServer().getPluginManager().callEvent(event);
-            }
-        }.runTaskAsynchronously(FinalChat.instance);
-        return true;
+        AsyncPlayerChatEvent event = new AsyncPlayerChatEvent(true, player, argumentos.joinStringArgs(), onlinePlayer);
+
+        FCScheduller.runAssync(() -> Bukkit.getServer().getPluginManager().callEvent(event));
     }
 }
