50426e5963a280100812c2b393b6a48fff356e8a
Pétrus Pradella <petrus_ant@hotmail.com>
2024-03-24 04:31:02 -0300

    Fix chat spy error

--- src/main/java/br/com/finalcraft/finalchat/commands/CoreCommand.java
+++ src/main/java/br/com/finalcraft/finalchat/commands/CoreCommand.java
@@ -4,6 +4,9 @@ import br.com.finalcraft.evernifecore.argumento.MultiArgumentos;
 import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.Arg;
 import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.FinalCMD;
 import br.com.finalcraft.evernifecore.ecplugin.ECPluginManager;
+import br.com.finalcraft.evernifecore.locale.FCLocale;
+import br.com.finalcraft.evernifecore.locale.LocaleMessage;
+import br.com.finalcraft.evernifecore.locale.LocaleType;
 import br.com.finalcraft.finalchat.FinalChat;
 import br.com.finalcraft.finalchat.PermissionNodes;
 import br.com.finalcraft.finalchat.util.messages.SpyMessage;
@@ -12,23 +15,28 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @FinalCMD(
-        aliases = {"finalchat","fchat", "chat", "fancychat"},
+        aliases = {"finalchat","chat"},
         helpHeader = "§6(  §a§lFinalChat§e  §6)§m"
 )
 public class CoreCommand {
+
+    @FCLocale(lang = LocaleType.EN_US, text = "§6§l ▶ §aChatSpy Status: %spy_status%")
+    public static LocaleMessage CHAT_SPY_STATUS;
 
     @FinalCMD.SubCMD(
             subcmd = "spy",
             permission = PermissionNodes.COMMAND_SPY
     )
-    public void spy(Player player, String label, MultiArgumentos argumentos, @Arg(name = "[ON|OFF]") String operation, @Arg(name = "[color]") String color){
+    public void spy(Player player, MultiArgumentos argumentos, @Arg(name = "[ON|OFF]") String operation, @Arg(name = "[color]") String color){
 
         if (operation == null){
-            player.sendMessage("§6§l ▶ §aChatSpy Enabled: " + SpyMessage.isSpying(player));
+            CHAT_SPY_STATUS
+                    .addPlaceholder("%spy_status%", SpyMessage.isSpying(player) ? "§aAtivado" : "§cDesativado")
+                    .send(player);
             return;
         }
 
-        if (color != null){
+        if (color == null){
             color = "§7";
         }
 
